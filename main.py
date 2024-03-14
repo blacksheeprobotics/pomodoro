14713875ba0ed62d7ca8870a9ed90d4a5c427d6c
RJW <blacksheeprobotics@gmail.com>
2024-03-14 01:05:48 -0400

    Added timer label and picture canvas

--- main.py
+++ main.py
@@ -19,6 +19,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro Timer App")
-window.minsize(width=800, height=600)
+# window.minsize(width=800, height=600)
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Add Timer Label
+label_timer = tkinter.Label(text="Timer", font=(FONT_NAME, 40))
+label_timer.grid(row=0, column=1)
+label_timer.config(bg=YELLOW, fg=GREEN)
+
+
 
 window.mainloop()
